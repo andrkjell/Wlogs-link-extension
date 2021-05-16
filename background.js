dc9fc0bc219914d00c9f62b4a5c3c1f84bec235b
Luttners <Markus@luttnersdatakonsult.se>
2021-05-16 22:44:25 +0200

    Update background.js

---
+++ background.js
@@ -0,0 +1,7 @@
+    var proggressButton = document.getElementsByClassName("armoryLink");
+    var x = document.createElement("A");
+    var t = document.createTextNode("button");
+    x.setAttribute("href", "https://www.google.se/");
+    x.appendChild(t);
+    proggressButton.appendData(x);
+
