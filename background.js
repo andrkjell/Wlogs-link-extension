2ab2f8fdc7b3f00145c5e4782d5cef5d4576bb57
Luttners <Markus@luttnersdatakonsult.se>
2021-05-16 23:08:35 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
-    var proggressButton = document.getElementsByClassName("armoryLink");
-    var x = document.createElement("A");
-    var t = document.createTextNode("button");
-    x.setAttribute("href", "https://www.google.se/");
-    x.appendChild(t);
-    proggressButton.appendData(x);
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
+var url = window.location.href;
+var newURL = url.replace("wowprogress", "warcraftlogs");
+var proggressButton = document.getElementsByClassName("armoryLink")[0];
+var x = document.createElement("a");
+var t = document.createTextNode("  (warcraftlogs)");
+x.setAttribute("href", newURL);
+x.appendChild(t);
+insertAfter(proggressButton, x);
