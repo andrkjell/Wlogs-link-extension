ba300730fbb15908552ff68642e71b746946f7fd
andrkjell <97ankj96@gmail.com>
2021-05-20 14:34:59 +0200

    commented code abit

--- background.js
+++ background.js
@@ -2,9 +2,13 @@ function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+
+//GENERIC CODE
 var url = window.location.href;
 var newURL = url.replace("wowprogress.com", "warcraftlogs.com");
 var proggressButton = document.getElementsByClassName("armoryLink")[0];
+
+//WOWPROGRESS->WARCRAFTLOGS
 var x = document.createElement("a");
 var t = document.createTextNode("     (warcraftlogs)");
 x.setAttribute("href", newURL);
@@ -14,7 +18,7 @@ x.rel
 x.appendChild(t);
 insertAfter(proggressButton, x);
 
-
+//WOWPROGRESS->RAIDERIO
 var newURLIO = url.replace("https://www.wowprogress.com/character/", "https://raider.io/characters/");
 var y = document.createElement("a");
 var k = document.createTextNode("     (RaiderIO)");
