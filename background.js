ca4a86e6eea1351b4f1433d5d651e08e08ec0bea
andrkjell <97ankj96@gmail.com>
2021-05-20 13:58:33 +0200

    added RaiderIO link, also added icons aswell as deleted content.js

--- background.js
+++ background.js
@@ -3,11 +3,20 @@ function insertAfter(referenceNode, newNode) {
 }
 
 var url = window.location.href;
-var newURL = url.replace("wowprogress", "warcraftlogs");
+var newURL = url.replace("wowprogress.com", "warcraftlogs.com");
 var proggressButton = document.getElementsByClassName("armoryLink")[0];
 var x = document.createElement("a");
-var t = document.createTextNode("  (warcraftlogs)");
+var t = document.createTextNode("     (warcraftlogs)");
 x.setAttribute("href", newURL);
 x.setAttribute("target", "_blank");
 x.appendChild(t);
 insertAfter(proggressButton, x);
+
+
+var newURLIO = url.replace("https://www.wowprogress.com/character/", "https://raider.io/characters/");
+var y = document.createElement("a");
+var k = document.createTextNode("     (RaiderIO)");
+y.setAttribute("href", newURLIO);
+y.setAttribute("target", "_blank");
+y.appendChild(k);
+insertAfter(proggressButton, y);
